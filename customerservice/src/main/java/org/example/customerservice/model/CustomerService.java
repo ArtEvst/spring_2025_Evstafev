4f9c4a34f51fcbd926329081330dee30c5e2f8f0
EvstafevArtem <evstafev.as@phystech.edu>
2025-03-16 14:50:38 +0300

    did CustomerServicePostgreController and some tests

--- customerservice/src/main/java/org/example/customerservice/model/CustomerService.java
+++ customerservice/src/main/java/org/example/customerservice/model/CustomerService.java
@@ -3,4 +3,7 @@ package org.example.customerservice.model;
 import org.springframework.data.annotation.Id;
 
 public record CustomerService(@Id Long id, String firstName, String lastName, Integer age, String phoneNumber) {
+    public CustomerService(Long id, CustomerService customerService){
+        this(id, customerService.firstName, customerService.lastName, customerService.age, customerService.phoneNumber);
+    }
 }
--- customerservice/src/test/java/org/example/customerservice/CustomerserviceApplicationTests.java
+++ customerservice/src/test/java/org/example/customerservice/CustomerserviceApplicationTests.java
@@ -1,13 +1,22 @@
 package org.example.customerservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class CustomerserviceApplicationTests {
+
+	@Autowired
+	WebTestClient webTestClient;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(webTestClient);
 	}
-
 }
